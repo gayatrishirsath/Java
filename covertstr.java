2d7ad39e36935fff1cac92a519606e70078bf306
Gayatri Shirsath <161936181+gayatrishirsath@users.noreply.github.com>
2025-03-08 15:53:40 +0530

    covertstr.java

---
+++ covertstr.java
@@ -0,0 +1,11 @@
+package gayatri;
+public class convertstr {
+
+	public static void main(String[] args) {
+		
+		String s1="Rbnb college shrirampur";
+		System.out.println(s1.toUpperCase());
+		System.out.println(s1.toLowerCase());
+	}
+
+}
