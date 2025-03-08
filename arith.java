66241bf064e02aab2b694c1cd0c8148098404a58
Gayatri Shirsath <161936181+gayatrishirsath@users.noreply.github.com>
2025-03-08 15:49:56 +0530

    arith.java

---
+++ arith.java
@@ -0,0 +1,19 @@
+package gayatri;
+
+public class arith
+{
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			int a=5;
+			int b=0;
+			int c=a/b;
+		}catch(Exception e)
+		{
+			System.out.println("Error ali print keli="+e);
+		}
+	}
+
+}
