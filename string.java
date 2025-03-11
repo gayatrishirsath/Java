3f9827f963d5f1e8fa271a331b69da7e4b4d206c
Gayatri Shirsath <161936181+gayatrishirsath@users.noreply.github.com>
2025-03-11 10:37:48 +0530

    string.java

---
+++ string.java
@@ -0,0 +1,20 @@
+package gayatri;
+
+public class string {
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			String s="shrirampur";
+			
+			System.out.println("character="+s.charAt(11));
+		}catch(Exception e)
+		{
+			System.out.println("Error=!"+e);	
+		}
+
+
+	}
+
+}
