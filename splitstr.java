d1cc6053b1eee8f9d6c0a9d5fb2d74c5801fced3
Gayatri Shirsath <161936181+gayatrishirsath@users.noreply.github.com>
2025-03-11 10:33:53 +0530

    splitstr.java

---
+++ splitstr.java
@@ -0,0 +1,15 @@
+package gayatri;
+
+public class splitstr {
+
+	public static void main(String[] args) {
+
+		String s1="Rbnb college shrirampur";
+		String s2[]=s1.split(" ");
+        for(int i=0;i<s2.length;i++)
+        {
+        	System.out.println(s2[i]);
+        }
+	}
+
+}
