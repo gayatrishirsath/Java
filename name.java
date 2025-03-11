b6a56c7532a9be953a6ea35a6c3d2d5c1423ccb3
Gayatri Shirsath <161936181+gayatrishirsath@users.noreply.github.com>
2025-03-11 10:35:55 +0530

    name.java

---
+++ name.java
@@ -0,0 +1,20 @@
+package gayatri;
+
+public class num {
+
+	public static void main(String[] args) {
+
+		try
+		{
+			String s="10G";
+			int n=Integer.parseInt(s);
+			System.out.println("value="+n);
+		}catch(Exception e)
+		{
+			System.out.println("Error=!"+e);	
+		}
+
+
+	}
+
+}
